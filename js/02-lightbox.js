ae2ca2842ca246bca906773705e5e1007c645633
Androogs <139003434+Androogs@users.noreply.github.com>
2024-01-23 23:07:22 -0500

    Add files via upload

---
+++ js/02-lightbox.js
@@ -0,0 +1,30 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+let gallery = document.querySelector(".gallery");
+
+const newImages = [...galleryItems]
+  .map(
+    (galleryItem) =>
+      `
+<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" title="${galleryItem.description}"/>
+   </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", newImages);
+
+gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  gallery.options.CaptionDelay = 250;
+});
+gallery.on("error.simplelightbox", function (e) {
+  console.log(e); // Some usefull information
+});
+
+console.log(galleryItems);
